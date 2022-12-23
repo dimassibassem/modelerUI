8a0c019e2a0e0181ca23e8ca1e022a70259707d4
Bassem Dimassi <dimassibassem99@gmail.com>
2022-12-23 17:42:24 +0100

    :sparkles: Inspiration

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import {
 } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
