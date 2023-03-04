7d50c649cf8efc03403ab65bad57e282add58ed8
Bassem Dimassi <dimassibassem99@gmail.com>
2023-03-04 01:36:03 +0100

    :heavy_minus_sign: delete unnecessary files

--- Client/src/hooks/useOnConnectEdge.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction, useCallback } from 'react'
-import { addEdge, Connection, Edge } from 'reactflow'
-
-function useOnConnectEdge(setEdgesArray: Dispatch<SetStateAction<Edge[]>>) {
-  return useCallback((connection: Connection) => setEdgesArray((eds) => addEdge(connection, eds)), [setEdgesArray])
-}
-
-export default useOnConnectEdge
