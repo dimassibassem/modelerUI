11ab9bac655f4394aa6c7b0a07ae32e2f7c9f87d
Bassem Dimassi <dimassibassem99@gmail.com>
2023-03-13 23:23:18 +0100

    :heavy_minus_sign: Remove vite-plugin-pages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,21 @@
-import { Suspense } from 'react'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Flow from '../src/pages/dev/flow'
+import EasyConnectExample from '../src/pages/dev/index'
+import NotFound from '../src/pages/404'
 
-import {
-  useRoutes,
-} from 'react-router-dom'
-import routes from '~react-pages'
+const DnDFlow = lazy(() => import('../src/pages/index'))
 
 const App = () => (
-    <Suspense fallback={<p>Loading...</p>}>
-      {useRoutes(routes)}
-    </Suspense>
-  )
+  <Suspense fallback={<p>Loading...</p>}>
+    <Router>
+      <Routes>
+        <Route index element={<DnDFlow />} />
+        <Route path='dev/flow' element={<Flow />} />
+        <Route path='dev' element={<EasyConnectExample />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
+  </Suspense>
+)
 export default App
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Pages from 'vite-plugin-pages'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    Pages({
-      dirs: 'src/pages'
-    })],
+  plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': {
+      API_URL: JSON.stringify(process.env.API_URL)
+    }
   }
 })
+
