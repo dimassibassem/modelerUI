199b33669ab08dc24f5b5e04a5c727ed8081ba34
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-02 23:46:38 +0100

    :wrench: cypress config

---
+++ Client/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  experimentalSourceRewriting: false,
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: false
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite'
+    },
+    screenshotOnRunFailure: false,
+    video: false
+  }
+})
