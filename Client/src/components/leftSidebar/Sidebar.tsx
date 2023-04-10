199075e63e170a874627b5959563521cc5f3c4ff
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-10 11:51:34 +0100

    onLoad Image

--- Client/src/components/leftSidebar/Sidebar.tsx
+++ Client/src/components/leftSidebar/Sidebar.tsx
@@ -1,10 +1,11 @@
-import React, { useState, DragEvent, MouseEvent, FocusEvent } from 'react'
-import { Link } from 'react-router-dom'
-import { isEdge } from 'react-device-detect'
-import logoBankerise from '@/assets/logo-bankerise.png'
-import iconSwitcher from '@/components/leftSidebar/iconSwitcher'
-import NodeTypes from '@/types/NodeTypes'
-import classNames from '@/utils/classNames'
+import React, { useState, DragEvent, MouseEvent, FocusEvent } from "react";
+import { Link } from "react-router-dom";
+import { isEdge } from "react-device-detect";
+import { useEffectOnce } from "usehooks-ts";
+import logoBankerise from "@/assets/logo-bankerise.png";
+import iconSwitcher from "@/components/leftSidebar/iconSwitcher";
+import NodeTypes from "@/types/NodeTypes";
+import classNames from "@/utils/classNames";
 
 const shapes = [
   NodeTypes.Start,
@@ -13,57 +14,66 @@ const shapes = [
   NodeTypes.Execution,
   NodeTypes.Provisioners,
   NodeTypes.Rule
-]
+];
 
 const onDragStart = (
   event: DragEvent<HTMLDivElement>,
   nodeType: NodeTypes,
   previewImage: string
 ) => {
-  const img = new Image()
-  img.src = previewImage
-  event.dataTransfer.setData('application/reactflow', nodeType)
-  event.dataTransfer.effectAllowed = 'move'
+  const img = new Image();
+  img.src = previewImage;
+  event.dataTransfer.setData("application/reactflow", nodeType);
+  event.dataTransfer.effectAllowed = "move";
   if (isEdge) {
-    event.dataTransfer.setDragImage(img, 10, 10)
+    event.dataTransfer.setDragImage(img, 10, 10);
   }
 
   // @ts-ignore
   // event.dataTransfer.forceFallback = true
-}
+};
 
 const handlePreviewDragImage = (
   event: MouseEvent<HTMLDivElement> | FocusEvent<HTMLDivElement>,
   setPreviewImage: (previewImage: string) => void
 ) => {
-  event.preventDefault()
-  const svgElement = (event.target as Element).querySelector<SVGElement>('svg')
+  event.preventDefault();
+  const svgElement = (event.target as Element).querySelector<SVGElement>("svg");
   if (svgElement) {
-    const svgCopy = svgElement.cloneNode(true) as SVGElement
-    svgCopy?.setAttribute('width', '80')
-    svgCopy?.setAttribute('height', '80')
-    const img = new Image()
-    img.src = `data:image/svg+xml;utf8,${encodeURIComponent(svgCopy.outerHTML)}`
-    setPreviewImage(img.src)
+    const svgCopy = svgElement.cloneNode(true) as SVGElement;
+    svgCopy?.setAttribute("width", "80");
+    svgCopy?.setAttribute("height", "80");
+    const img = new Image();
+    img.src = `data:image/svg+xml;utf8,${encodeURIComponent(svgCopy.outerHTML)}`;
+    setPreviewImage(img.src);
   }
-}
+};
+
 
 const Sidebar = () => {
-  const [expanded, setExpanded] = useState(true)
-  const toggleExpanded = () => setExpanded(!expanded)
-  const [previewImage, setPreviewImage] = useState<string>('')
-
+  const [expanded, setExpanded] = useState(true);
+  const toggleExpanded = () => setExpanded(!expanded);
+  const [previewImage, setPreviewImage] = useState<string>("");
+  const [imageLoaded, setImageLoaded] = useState(false);
+  useEffectOnce(() => {
+    const img = new Image();
+    img.onload = () => setImageLoaded(true);
+    img.src = logoBankerise;
+  });
   return (
     <div className="min-h-0 flex-1 flex-col border-r border-gray-200 bg-white max-w-[20%] hidden lg:flex ">
       <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4  ">
         <div className="flex flex-shrink-0 items-center px-4">
-          <img
+          {imageLoaded ? <img
             className="h-16 w-auto"
             src={logoBankerise}
             height="50"
             width="50"
             alt="Bankerise"
-          />
+          /> : <img
+            src="https://via.placeholder.com/150"
+            alt="thumbnail"
+          />}
         </div>
         <nav className="mt-5 flex-1" aria-label="Sidebar">
           <button
@@ -71,9 +81,9 @@ const Sidebar = () => {
             onClick={toggleExpanded}
             className={classNames(
               expanded
-                ? 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
-                : 'bg-gray-100 text-gray-900',
-              'pl-3 ml-2 text-gray-600 hover:text-gray-900 items-center p-2 text-sm font-medium rounded-md flex w-full max-w-[90%]'
+                ? "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                : "bg-gray-100 text-gray-900",
+              "pl-3 ml-2 text-gray-600 hover:text-gray-900 items-center p-2 text-sm font-medium rounded-md flex w-full max-w-[90%]"
             )}
           >
             Shapes
@@ -83,25 +93,25 @@ const Sidebar = () => {
               {shapes.map((item) => (
                 <div
                   onMouseOver={(event: MouseEvent<HTMLDivElement>) => {
-                    handlePreviewDragImage(event, setPreviewImage)
+                    handlePreviewDragImage(event, setPreviewImage);
                   }}
                   onFocus={(event: FocusEvent<HTMLDivElement>) => {
-                    handlePreviewDragImage(event, setPreviewImage)
+                    handlePreviewDragImage(event, setPreviewImage);
                   }}
                   key={item}
                   className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
                   onDragStart={(event) => {
-                    onDragStart(event, item, previewImage)
+                    onDragStart(event, item, previewImage);
                   }}
                   onDrag={(event) => {
-                    event.preventDefault()
-                    const target = event.target as HTMLElement
-                    target.style.opacity = '0.5'
+                    event.preventDefault();
+                    const target = event.target as HTMLElement;
+                    target.style.opacity = "0.5";
                   }}
                   onDragEnd={(event) => {
-                    event.preventDefault()
-                    const target = event.target as HTMLElement
-                    target.style.opacity = '1'
+                    event.preventDefault();
+                    const target = event.target as HTMLElement;
+                    target.style.opacity = "1";
                   }}
                   draggable="true"
                 >
@@ -140,7 +150,7 @@ const Sidebar = () => {
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
