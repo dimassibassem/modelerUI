105ebc0f3f9ae248389613310fe8edc115b4c7aa
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-12 18:11:28 +0100

    :recycle: Refactoring Code

--- Client/src/components/rightSidebar/EdgeProps/SelectedEdgeProps.tsx
+++ Client/src/components/rightSidebar/EdgeProps/SelectedEdgeProps.tsx
@@ -110,7 +110,7 @@ const SelectedEdgeProps = () => {
       </label>
       <div className="grid grid-cols-4 gap-4">
         <input
-          type="label"
+          type="text"
           id="label"
           className="block p-1 w-full col-span-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
           placeholder="Add a Label"
