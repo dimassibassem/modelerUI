4204dffc64e49794e2cd294f9df793011f826052
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-13 09:03:12 +0100

    :wrench: Some Manual Chunks

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,21 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          reactFlow: ["reactflow"],
+        }
+      }
+    }
+  },
   define: {
-    'process.env': {
+    "process.env": {
       API_URL: JSON.stringify(process.env.API_URL)
     },
-    global: 'window'
+    global: "window"
   },
   resolve: {
     alias: {
-      '@': `${__dirname}/src`
+      "@": `${__dirname}/src`
     }
   }
-})
+});
