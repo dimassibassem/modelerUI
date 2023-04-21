e99e2d59d4389e0602c87f6ad1e25e18e5bdf935
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-21 05:12:58 +0100

    :adhesive_bandage: Adapting new changes

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,24 +1,23 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import multer from 'multer'
 import * as fs from 'fs'
 import { prisma } from './prisma'
 
-const app: Application = express()
 
+const app: Application = express()
 const port: number = 3001
 app.use(cors())
 app.use(express.static('uploaded_images'))
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.post('/api/add-model', async (req: Request, res: Response) => {
-
-  const { instance, dataURI } = req.body
-  // transform dataURI to blob
-  const blob = dataURItoBlob(dataURI)
-
-  await save(blob, instance)
-
+const upload = multer()
+app.post('/api/add-model', upload.single('flow'), async (req: Request, res: Response) => {
+  const { instance, process } = req.body
+  const file = req.file.buffer
+  console.log(JSON.parse(process))
+  await save(file, instance, process)
   res.send('Model Saved!')
 })
 
@@ -27,46 +26,33 @@ app.get('/api/get-models', async (req: Request, res: Response) => {
   models.forEach(model => {
     model.instance = JSON.parse(model.instance)
     model.fileName = `http://localhost:3001/${model.fileName}`
+    model.process = JSON.parse(model.process)
   })
   res.send(models)
 })
 
-function dataURItoBlob(dataURI) {
-  // convert base64 to raw binary data held in a string
-  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-  const byteString = atob(dataURI.split(',')[1])
-
-  // separate out the mime component
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-  // write the bytes of the string to an ArrayBuffer
-  const ab: any = new ArrayBuffer(byteString.length)
-
-  // create a view into the buffer
-  const ia = new Uint8Array(ab)
-
-  // set the bytes of the buffer to the correct values
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i)
-  }
-
-  // write the ArrayBuffer to a blob, and you're done
-  return new Blob([ab], { type: mimeString })
-
-}
-
-async function save(blob, instance) {
-
-  const buffer = Buffer.from(await blob.arrayBuffer())
+async function save(file, instance, process) {
   const fileName = `model-${Date.now()}.png`
-  await prisma.model.create({
-    data: {
-      fileName,
-      instance: JSON.stringify(instance)
-    }
-  })
-  fs.writeFile(`uploaded_images/${fileName}`, buffer, () => console.log('model saved!'))
 
+  if (!fs.existsSync('uploaded_images')) {
+    fs.mkdirSync('uploaded_images')
+  }
+  try {
+    fs.writeFile(`uploaded_images/${fileName}`, file, (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+    await prisma.model.create({
+      data: {
+        fileName,
+        instance,
+        process
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 app.listen(port, function() {
