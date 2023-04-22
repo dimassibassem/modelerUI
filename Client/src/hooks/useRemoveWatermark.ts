7e542716d2dc676cc01e7fdb976a5f75780266da
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-22 20:07:36 +0100

    :heavy_minus_sign: Removing reactflow minimap tooltip watermark

--- Client/src/hooks/useRemoveWatermark.ts
+++ Client/src/hooks/useRemoveWatermark.ts
@@ -2,11 +2,14 @@ import { useEffect } from 'react'
 
 function useRemoveWatermark() {
   return useEffect(() => {
+    // Remove reactflow watermark
     document
       .querySelector(
         'div.react-flow__panel.react-flow__attribution.bottom.right'
       )
       ?.remove()
+    // Remove reactflow minimap watermark
+    document.querySelector('#react-flow__minimap-desc-1')?.remove()
   }, [])
 }
 
