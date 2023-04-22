1b5d6e25eb776325da406291b5abdf3471a78335
Bassem Dimassi <dimassibassem99@gmail.com>
2023-04-22 23:59:06 +0100

    :recycle: Refactoring Code

---
+++ Client/src/style/tooltip.ts
@@ -0,0 +1,11 @@
+const tooltipStyle = {
+  backgroundColor: '#e0e7ff',
+  color: '#4f46e5',
+  borderRadius: '0.375rem',
+  padding: '0.5rem',
+  fontSize: '1rem',
+  lineHeight: '1.5rem',
+  fontWeight: 500
+}
+
+export default tooltipStyle
