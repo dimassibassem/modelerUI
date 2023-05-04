89a4565d4cd94a031f70c5bc7333bffbe40a1994
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-04 14:21:01 +0100

    :pencil2: Typos

--- Client/src/utils/Flow/saveModel.ts
+++ Client/src/utils/Flow/saveModel.ts
@@ -11,49 +11,52 @@ async function saveModel(
   setModelID: (id: number) => void,
   setOpenNotification: (open: boolean) => void
 ) {
-  const res = await imageFromHTML(reactFlowInstance)
-  if (res) {
-    const blob = await fetch(res.dataURI).then((r) => r.blob())
-    const file = new File([blob], 'flow.png', { type: 'image/png' })
-    const result = new FormData()
-    result.append('flow', file)
-    result.append('instance', JSON.stringify(res.instance))
-    result.append('process', JSON.stringify(process))
-    try {
-      if (!modelID) {
-        const response = await axios.post(
-          `${import.meta.env.VITE_API_ENDPOINT}/api/add-model`,
-          result,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data'
+  if (reactFlowInstance) {
+    reactFlowInstance.fitView()
+    const res = await imageFromHTML(reactFlowInstance)
+    if (res) {
+      const blob = await fetch(res.dataURI).then((r) => r.blob())
+      const file = new File([blob], 'flow.png', { type: 'image/png' })
+      const formData = new FormData()
+      formData.append('flow', file)
+      formData.append('instance', JSON.stringify(res.instance))
+      formData.append('process', JSON.stringify(process))
+      try {
+        if (!modelID) {
+          const response = await axios.post(
+            `${import.meta.env.VITE_API_ENDPOINT}/api/add-model`,
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
             }
-          }
-        )
-        setModelID(response.data.id)
-        setNotificationData({
-          success: true,
-          message: 'Model saved successfully'
-        })
-        setOpenNotification(true)
-      } else {
-        await axios.put(
-          `${import.meta.env.VITE_API_ENDPOINT}/api/update-model/${modelID}`,
-          result,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data'
+          )
+          setModelID(response.data.id)
+          setNotificationData({
+            success: true,
+            message: 'Model saved successfully'
+          })
+          setOpenNotification(true)
+        } else {
+          await axios.put(
+            `${import.meta.env.VITE_API_ENDPOINT}/api/update-model/${modelID}`,
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
             }
-          }
-        )
-        setNotificationData({
-          success: true,
-          message: 'Model updated successfully'
-        })
-        setOpenNotification(true)
+          )
+          setNotificationData({
+            success: true,
+            message: 'Model updated successfully'
+          })
+          setOpenNotification(true)
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
     }
   }
 }
