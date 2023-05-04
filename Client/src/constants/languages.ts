a0541bdade7603566c7ab93dec0b84e8c38d6814
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-04 14:19:07 +0100

    :wrench: Handling language change

--- Client/src/constants/languages.ts
+++ Client/src/constants/languages.ts
@@ -1,6 +1,6 @@
 const languages = [
-  { id: 1, name: 'English', code: 'en' },
-  { id: 2, name: 'French', code: 'fr' }
+  { id: 1, name: 'English', code: 'en', icon: 'flagpack:gb-ukm' },
+  { id: 2, name: 'French', code: 'fr', icon: 'flagpack:fr' }
 ]
 
 export default languages
--- Client/src/hooks/useHandleLanguageChange.ts
+++ Client/src/hooks/useHandleLanguageChange.ts
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import useLocalStorage from '@/store/localStorage'
+import dayjs from 'dayjs'
 
 const useHandleLangChange = () => {
   const { i18n } = useTranslation()
   const lang = useLocalStorage((store) => store.lang)
   return useEffect(() => {
     i18n.changeLanguage(lang)
+    dayjs.locale(lang)
   }, [i18n, lang])
 }
 
