d46bafc9f2f16f5a7b41f709fb3e466937042f4a
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-04 16:07:45 +0100

    :construction: Work in Progress

--- Client/src/components/Policies/Details.tsx
+++ Client/src/components/Policies/Details.tsx
@@ -7,10 +7,10 @@ import capitalize from '@/utils/capitalize'
 import dayjs from 'dayjs'
 
 const Details = ({
-  open,
-  setOpen,
-  model
-}: {
+                   open,
+                   setOpen,
+                   model
+                 }: {
   open: boolean
   setOpen: (open: boolean) => void
   model: Model | null
@@ -19,169 +19,167 @@ const Details = ({
   const id = useId()
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Dialog as='div' className='relative z-10' onClose={setOpen}>
         <Transition.Child
           as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          enter='ease-in-out duration-500'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in-out duration-500'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
         </Transition.Child>
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+        <div className='fixed inset-0 overflow-hidden'>
+          <div className='absolute inset-0 overflow-hidden'>
+            <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10'>
               <Transition.Child
                 as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
+                enter='transform transition ease-in-out duration-500 sm:duration-700'
+                enterFrom='translate-x-full'
+                enterTo='translate-x-0'
+                leave='transform transition ease-in-out duration-500 sm:duration-700'
+                leaveFrom='translate-x-0'
+                leaveTo='translate-x-full'
               >
-                <Dialog.Panel className="pointer-events-auto relative w-96">
+                <Dialog.Panel className='pointer-events-auto relative w-96'>
                   <Transition.Child
                     as={Fragment}
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
+                    enter='ease-in-out duration-500'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in-out duration-500'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
                   >
-                    <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+                    <div className='absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4'>
                       <button
-                        type="button"
-                        className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                        type='button'
+                        className='rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white'
                         onClick={() => setOpen(false)}
                       >
-                        <span className="sr-only">Close panel</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        <span className='sr-only'>Close panel</span>
+                        <XMarkIcon className='h-6 w-6' aria-hidden='true' />
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="h-full overflow-y-auto bg-white p-8">
-                    <div className="space-y-6 pb-16">
+                  <div className='h-full overflow-y-auto bg-white p-8'>
+                    <div className='space-y-6 pb-16'>
                       <div>
-                        <div className="aspect-h-7 aspect-w-10 block w-full shadow overflow-hidden rounded-lg">
+                        <div className='aspect-h-7 aspect-w-10 block w-full shadow overflow-hidden rounded-lg'>
                           <img
                             src={model?.fileName}
-                            alt=""
-                            className="object-cover"
+                            alt=''
+                            className='object-cover'
                           />
                         </div>
-                        <div className="mt-4 flex items-start justify-between">
-                          <h2 className="text-base font-semibold leading-6 text-gray-900">
-                            <span className="sr-only">Details for </span>
+                        <div className='mt-4 flex items-start justify-between'>
+                          <h2 className='text-base font-semibold leading-6 text-gray-900'>
+                            <span className='sr-only'>Details for </span>
                             {model?.process.name}
                           </h2>
                         </div>
                       </div>
                       <div>
-                        <h3 className="font-medium text-gray-900">
+                        <h3 className='font-medium text-gray-900'>
                           Information
                         </h3>
-                        <dl className="mt-2 divide-y divide-gray-200 border-b border-t border-gray-200">
-                          <div className="flex justify-between py-3 text-sm font-medium">
-                            <dt className="text-gray-500">Created</dt>
-                            <dd className="text-gray-900">
+                        <dl className='mt-2 divide-y divide-gray-200 border-b border-t border-gray-200'>
+                          <div className='flex justify-between py-3 text-sm font-medium'>
+                            <dt className='text-gray-500'>Created</dt>
+                            <dd className='text-gray-900'>
                               {dayjs(model?.createdAt).format(
                                 'DD MMMM YYYY, h:mm:ss a'
                               )}
                             </dd>
                           </div>
-                          <div className="flex justify-between py-3 text-sm font-medium">
-                            <dt className="text-gray-500">Last modified</dt>
-                            <dd className="text-gray-900">
+                          <div className='flex justify-between py-3 text-sm font-medium'>
+                            <dt className='text-gray-500'>Last modified</dt>
+                            <dd className='text-gray-900'>
                               {dayjs(model?.updatedAt).format(
                                 'DD MMMM YYYY, h:mm:ss a'
                               )}
                             </dd>
                           </div>
-                          <div className="flex justify-between py-3 text-sm font-medium">
-                            <dt className="text-gray-500">Hook Name</dt>
-                            <dd className="text-gray-900">
+                          <div className='flex justify-between py-3 text-sm font-medium'>
+                            <dt className='text-gray-500'>Hook Name</dt>
+                            <dd className='text-gray-900'>
                               {model?.process.hook.name}
                             </dd>
                           </div>
-                          <div className="flex justify-between py-3 text-sm font-medium">
-                            <dt className="text-gray-500">Channel</dt>
-                            <dd className="text-gray-900">
+                          <div className='flex justify-between py-3 text-sm font-medium'>
+                            <dt className='text-gray-500'>Channel</dt>
+                            <dd className='text-gray-900'>
                               {model?.process.hook.channel}
                             </dd>
                           </div>
-                          <div className="flex justify-between py-3 text-sm font-medium">
-                            <dt className="text-gray-500">IsAsync</dt>
-                            <dd className="text-gray-900">
+                          <div className='flex justify-between py-3 text-sm font-medium'>
+                            <dt className='text-gray-500'>IsAsync</dt>
+                            <dd className='text-gray-900'>
                               {model?.process.hook.isAsync ? 'Yes' : 'No'}
                             </dd>
                           </div>
                         </dl>
                       </div>
                       <div>
-                        <h3 className="font-medium text-gray-900">
+                        <h3 className='font-medium text-gray-900'>
                           Description
                         </h3>
-                        <div className="mt-2 flex items-center justify-between">
-                          <p className="text-sm italic text-gray-500">
+                        <div className='mt-2 flex items-center justify-between'>
+                          <p className='text-sm italic text-gray-500'>
                             {model?.process.description}
                           </p>
                         </div>
                       </div>
                       <div>
-                        <h3 className="font-medium text-gray-900">Stages</h3>
+                        <h3 className='font-medium text-gray-900'>Stages</h3>
                         {model?.process.steps.map((step, i) => (
-                          <div key={`${id + i}`} className="mt-2">
-                            <h2 className=" font-medium text-gray-900">
+                          <div key={`${id + i}`} className='mt-2'>
+                            <h2 className=' font-medium text-gray-900'>
                               Combination {i + 1}
                             </h2>
-                            <dl className="mt-2 divide-y divide-gray-200 border-b border-t border-gray-200">
+                            <dl className='mt-2 divide-y divide-gray-200 border-b border-t border-gray-200'>
                               {step?.map((stage, index) => (
                                 <div key={`${index + stage.id}`}>
-                                  <dt className="font-medium">
+                                  <dt className='font-medium'>
                                     {capitalize(stage.type)}
                                   </dt>
-                                  {stage.attributes && (
-                                    <div>
-                                      {Object.keys(stage.attributes).map(
-                                        (key) => (
-                                          <div
-                                            key={stage.id + key}
-                                            className="flex justify-between py-3 text-sm font-medium"
-                                          >
-                                            <dt className="text-gray-500">
-                                              {capitalize(key)}
-                                            </dt>
-                                            <dd className="text-gray-900">
-                                              {stage.attributes?.[key]}
-                                            </dd>
-                                          </div>
-                                        )
-                                      )}
-                                    </div>
-                                  )}
+                                  <div>
+                                    {Object.keys(stage.attributes).map(
+                                      (key) => (
+                                        <div
+                                          key={stage.id + key}
+                                          className='flex justify-between py-3 text-sm font-medium'
+                                        >
+                                          <dt className='text-gray-500'>
+                                            {capitalize(key)}
+                                          </dt>
+                                          <dd className='text-gray-900'>
+                                            {stage.attributes?.[key]}
+                                          </dd>
+                                        </div>
+                                      )
+                                    )}
+                                  </div>
                                 </div>
                               ))}
                             </dl>
                           </div>
                         ))}
                       </div>
-                      <div className="flex">
+                      <div className='flex'>
                         <button
-                          type="button"
-                          className="flex-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                          type='button'
+                          className='flex-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                           onClick={() => navigate(`/modeler/${model?.id}`)}
                         >
                           Edit
                         </button>
                         <button
-                          type="button"
-                          className="ml-3 flex-1 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                          type='button'
+                          className='ml-3 flex-1 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
                         >
                           Delete
                         </button>
