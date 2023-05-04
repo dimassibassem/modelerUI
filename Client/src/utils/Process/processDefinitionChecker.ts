904cfc23daf381b3e55f79317cbf5657372310c4
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-04 14:21:46 +0100

    :heavy_minus_sign: Deleting Start & End nodes from steps array

--- Client/src/utils/Process/processDefinitionChecker.ts
+++ Client/src/utils/Process/processDefinitionChecker.ts
@@ -1,6 +1,7 @@
 import { Edge, Node } from 'reactflow'
 import { createGraph, findAllPaths } from '@/utils/Flow/graphPath'
 import Process from '@/types/Process'
+import NodeType from '@/types/NodeType'
 
 const processDefinitionChecker = (
   nodes: Node[],
@@ -8,35 +9,50 @@ const processDefinitionChecker = (
   setProcess: (process: Process) => void,
   process: Process
 ) => {
-  const startNode = nodes.find((node) => node.type === 'start')
-  const endNode = nodes.find((node) => node.type === 'end')
+  const startNode = nodes.find((node) => node.type === NodeType.Start)
+  const endNode = nodes.find((node) => node.type === NodeType.End)
+
   if (!startNode || !endNode) {
-    if (process.steps.length > 0)
-      setProcess({ ...process, steps: [] } as Process)
+    if (process.steps.length > 0) {
+      setProcess({ ...process, steps: [] })
+    }
     return
   }
+
   const graph = createGraph(nodes, edges)
   const paths = findAllPaths(graph, startNode.id, endNode.id)
+
   if (paths.length === 0) {
-    if (process.steps.length > 0)
-      setProcess({ ...process, steps: [] } as Process)
+    if (process.steps.length > 0) {
+      setProcess({ ...process, steps: [] })
+    }
     return
   }
-  const steps = paths.map((path) =>
-    path.map((nodeId) => {
-      const node = nodes.find((nd) => nd.id === nodeId)
-      return node?.type === 'start' || node?.type === 'end'
-        ? {
-            id: node?.id,
-            type: node?.type
+
+  const steps = paths.map((path) => {
+    return path
+      .map((nodeId) => {
+        const node = nodes.find((nd) => nd.id === nodeId)
+        if (node) {
+          return {
+            id: node.id,
+            type: node.type,
+            attributes: node.data.attributes
           }
-        : {
-            id: node?.id,
-            type: node?.type,
-            attributes: node?.data.attributes
-          }
-    })
-  )
+        }
+      })
+      .filter((step) => step !== undefined)
+  })
+
+  steps.forEach((step) => {
+    if (step[0]?.type === NodeType.Start) {
+      step.shift()
+    }
+    if (step[step.length - 1]?.type === NodeType.End) {
+      step.pop()
+    }
+  })
+
   setProcess({ ...process, steps } as Process)
 }
 
