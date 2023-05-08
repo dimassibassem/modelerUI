3da631e8385b612498aa8bb1531ba2c3840993cd
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-08 12:31:27 +0100

    :rotating_light: clear Eslint warns

--- Client/src/hooks/useHandleLanguageChange.ts
+++ Client/src/hooks/useHandleLanguageChange.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import useLocalStorage from '@/store/localStorage'
 import dayjs from 'dayjs'
+import useLocalStorage from '@/store/localStorage'
 
 const useHandleLangChange = () => {
   const { i18n } = useTranslation()
