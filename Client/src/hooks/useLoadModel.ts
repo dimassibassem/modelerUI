2654f0e4bdca472fad255b97c38e58313a2ca7de
Bassem Dimassi <dimassibassem99@gmail.com>
2023-05-08 09:56:26 +0100

    :bug: Fixing last node id number

--- Client/src/hooks/useLoadModel.ts
+++ Client/src/hooks/useLoadModel.ts
@@ -19,18 +19,20 @@ const selector = (state: RFState) => ({
   setProcess: state.setProcess
 })
 const selector2 = (state: State) => ({
-  setModelID: state.setModelID
+  setModelID: state.setModelID,
+  setLastNodeIdNumber: state.setLastNodeIdNumber
 })
 const useLoadModel = (setLoaded: (loaded: boolean) => void) => {
   const { id } = useParams<{ id: string }>()
   const { setProcess, setNodes, setEdges } = useFlowStore(selector, shallow)
-  const { setModelID } = useStore(selector2, shallow)
+  const { setModelID,setLastNodeIdNumber } = useStore(selector2, shallow)
   const navigate = useNavigate()
   return useEffect(() => {
     if (id) {
       loadModel(id)
         .then((data) => {
           setModelID(Number(id))
+          setLastNodeIdNumber(data.instance.nodes.length)
           setNodes(data.instance.nodes)
           setEdges(data.instance.edges)
           setProcess(data.process)
